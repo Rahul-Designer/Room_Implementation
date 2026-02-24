def4ff669555abe25a10e9643c68a73c4caa79d1
Rahul Singh <singhrahul50888@gmail.com>
2026-02-24 23:23:28 +0530

    Add scaffold

--- app/src/main/java/com/company/room_implementation/LoginScreen.kt
+++ app/src/main/java/com/company/room_implementation/LoginScreen.kt
@@ -1,8 +1,14 @@
 package com.company.room_implementation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun LoginScreen() {
-
+    Scaffold(modifier = Modifier) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) { }
+    }
 }
